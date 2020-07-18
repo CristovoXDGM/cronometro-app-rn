2fa6f550613c87ddcaf73a5992e3870352354afe
cristovoxdgm <cristovoxdgm@gmail.com>
2020-07-18 15:32:26 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function App() {
+
+  const [timer, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function startTimer() {
+    setIsActive(!isActive);
+  }
+
+  function cleanTimer() {
+    setTimer(0.0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = 1000;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer(timer => timer + 0.1);
+      }, 100);
+    } else if (!isActive && timer !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, timer]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image source={require("./src/cronometro.png")} style={styles.img} />
+      <Text style={styles.counterText} >{timer.toFixed(1)}</Text>
+      <View style={styles.goView}>
+        <TouchableOpacity style={styles.goButton} onPress={startTimer}>
+          <Text style={styles.goTextButton} >Go</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.goButton}>
+          <Text style={styles.goTextButton} onPress={cleanTimer} >Clear/Stop</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -18,4 +53,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  counterText: {
+    margin: -100,
+    fontSize: 50,
+    fontWeight: "bold"
+  },
+  goButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: Dimensions.get("window").width / 2.5,
+
+    backgroundColor: "#000",
+    margin: 5,
+    borderRadius: 25
+  },
+
+  goTextButton: {
+    color: "#f5f5f5",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  goView: {
+    flexDirection: "row",
+    marginTop: 150,
+    height: 60
+  },
+  img: {
+
+    height: Dimensions.get("window").height / 4,
+    width: Dimensions.get("window").width / 2.5
+  }
 });
